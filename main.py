c7c9324260709d705178ea761914f20a5e95a3b8
Juan Jose Duque Cardona <juanjo.dc@outlook.com>
2026-06-04 09:03:58 -0500

    implemented validations for entry blog

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI  
+from fastapi import FastAPI , HTTPException
+
+from pydantic import ValidationError
 from models import BlogEntry  
 
 app=FastAPI()  
@@ -8,5 +10,14 @@ async def get_root():
     return {"message":"Hello World"} 
 
 @app.post("/new_post") 
-async def create_post(entry:BlogEntry):
-    return {"post":entry}
+async def create_post(data:dict): 
+    try:
+        entry=BlogEntry.model_validate(data)
+        return {"message":"Post Has been created successfully"}
+    except ValidationError as e:  
+        raise HTTPException(
+            status_code=400,  
+            detail=f"model is missing the following data: {e.errors()}"
+        )
+
+    
