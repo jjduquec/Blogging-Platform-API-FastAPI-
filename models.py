7faf34a2970beb0e79d037adb3ff5b1b0902845c
Juan <juanjo.dc@outlook.com>
2026-06-21 15:24:30 -0500

    developing DB connection

--- models.py
+++ models.py
@@ -1,11 +1,10 @@
 from pydantic import BaseModel
 
 
-
 class BlogEntry(BaseModel): 
+    id:optional[int]=None
     title:str  
     content:str  
     category:str  
     tags:list  
-
-    
+#https://medium.com/@parth.khajgiwale/building-a-crud-api-with-fastapi-mysql-and-postman-8e849e62d9fe
